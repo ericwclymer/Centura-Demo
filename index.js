c11915ca0ddca0227cec55f13f7c8db63b1e4a1f
Eric Clymer <eric.clymer@hudl.com>
2018-03-10 16:29:16 -0600

    Step 3

---
+++ index.js
@@ -0,0 +1,5 @@
+// when the document has finished processing the resources in the <head> and loaded the body
+// run the code inside
+$(document).ready(function(){
+    console.log('The document has loaded');
+});
